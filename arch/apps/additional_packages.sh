fe509ae0c7011f9dc569973dac75c3097595cbf4
Ruslan Shmid <ruslan.shmid.rsd@gmail.com>
2021-11-29 11:14:38 +0200

    Change alacritty font

--- arch/apps/additional_packages.sh
+++ arch/apps/additional_packages.sh
@@ -3,7 +3,7 @@
 ### Fonts installation
 # sudo pacman -S terminus-font     --noconfirm
 # sudo pacman -S terminus-font-otb --noconfirm
-sudo yay -S FiraCode             --noconfirm
+yay -S ttf-consolas-ligaturized --noconfirm
 
 ### Install fonts in case of incorrect rendering japanese fonts
 # sudo pacman -S noto-fonts-cjk noto-fonts-emoji noto-fonts                                               --noconfirm
