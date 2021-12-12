c338c49c32c6ef65f746848c4ba9283972a484d7
Ruslan Shmid <ruslan.shmid.rsd@gmail.com>
2021-12-12 02:34:49 +0200

    Update fisher

--- arch/apps/after_sddm_startup.sh
+++ arch/apps/after_sddm_startup.sh
@@ -6,8 +6,17 @@ yay -S fisher --noconfirm
 
 fisher install FabioAntunes/fish-nvm # install fish nvm.
 fisher install jethrokuan/fzf        # install history search plugin
+fisher install jhillyerd/plugin-git  # git plugins
+fisher install acomagu/fish-async-prompt
+# fisher install jorgebucaran/spark.fish
 
 # install omf
-curl -L https://get.oh-my.fish | fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install default
 omf theme default
+
+# docs
+# https://github.com/jorgebucaran/cookbook.fish#how-do-i-create-a-function-in-fish
+# https://github.com/jorgebucaran/awsm.fish
+# https://github.com/jorgebucaran/getopts.fish
+# https://github.com/jorgebucaran/fishtape
